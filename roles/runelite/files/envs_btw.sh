6447e4ff8495363175830e993770c3f895e4203d
Shuhao Wu <shuhao@shuhaowu.com>
2018-12-14 19:14:01 -0500

    Forward pulse socket into the container

--- roles/runelite/files/envs_btw.sh
+++ roles/runelite/files/envs_btw.sh
@@ -1,2 +1,4 @@
 export RUNELITE_API_BASE="http://localhost:8080/runelite-"
 export RUNELITE_WS_BASE="wss://localhost:8080/runelite-"
+export RUNELITE_WS_BASE="wss://localhost:8080/runelite-"
+export PULSE_SERVER=unix:/tmp/.pulse-native
