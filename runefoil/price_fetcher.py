ab59ca5cef6697e74c39dc86408f9645c104bb2e
Shuhao Wu <shuhao@shuhaowu.com>
2018-12-16 00:16:11 -0500

    Fetch price more efficiently

--- runefoil/price_fetcher.py
+++ runefoil/price_fetcher.py
@@ -20,7 +20,6 @@ RL_ITEM_ICON_LARGE_URL = RL_ITEM_ICON_URL + "/large"
 
 class PriceFetcher(object):
   def __init__(self, rate_limit=1):
-    logging.basicConfig(format="[%(asctime)s][%(levelname)s] %(message)s", datefmt="%Y-%m-%d %H:%M:%S", level=logging.DEBUG)
     self.s = requests.Session()
     self.s.headers.update({"User-Agent": UA})
     self.rate_limit = rate_limit
@@ -35,12 +34,34 @@ class PriceFetcher(object):
   def __getattr__(self, attr):
     return getattr(self.s, attr)
 
-  def seed(self):
+  def fetch(self):
+    with self.dbconn.cursor() as c:
+      c.execute("SELECT COUNT(*) as cnt FROM `runelite`.`items`")
+      row_count = c.fetchone()["cnt"]
+
     r = self.get(RL_PRICES_URL)
     r.raise_for_status()
     all_prices = r.json()
 
+    if len(all_prices) != row_count:
+      logging.info("local cache has {} items, remote has {} items, calling seed instead.".format(row_count, len(all_prices)))
+      self.seed(all_prices=all_prices)
+      return
+
+    self._update_prices_table_and_commit(all_prices)
+
+  def seed(self, all_prices=None):
+    if all_prices is None:
+      r = self.get(RL_PRICES_URL)
+      r.raise_for_status()
+      all_prices = r.json()
+
     last_processed = datetime.datetime.now()
+
+    not_indb_count = 0
+    indb_count = 0
+
+    logging.info("seeding items table")
 
     for i, item_price in enumerate(all_prices):
       try:
@@ -66,22 +87,30 @@ class PriceFetcher(object):
 
             self._insert_item(item, c)
             indb = False
+            not_indb_count += 1
           else:
             indb = True
-
-          self._replace_price(item_price, c)
+            indb_count += 1
       except Exception:
         self.dbconn.rollback()
         raise
 
-      self.dbconn.commit()
-      logging.info("Processed {} (id={}, progress={}/{}, indb={})".format(item_price["name"], item_price["id"], i + 1, len(all_prices), indb))
+      logmethod = logging.debug
+      if i % 100 == 1:
+        logmethod = logging.info
+
+      logmethod("Processed {} (id={}, progress={}/{}, indb={})".format(item_price["name"], item_price["id"], i + 1, len(all_prices), indb))
 
       if not indb:
+        self.dbconn.commit()
         while (datetime.datetime.now() - last_processed).total_seconds() < self.rate_limit:
           time.sleep(0.02)
 
       last_processed = datetime.datetime.now()
+
+    logging.info("items table seeded with {} items added to db and {} existing items".format(not_indb_count, indb_count))
+
+    self._update_prices_table_and_commit(all_prices)
 
   def _select_item(self, item_id, cursor):
     cursor.execute("SELECT * FROM `runelite`.`items` WHERE id = %s", (item_id, ))
@@ -103,29 +132,35 @@ class PriceFetcher(object):
 
     cursor.execute("INSERT INTO `runelite`.`items` ({}) values ({})".format(columns, subs), args)
 
-  def _replace_price(self, price, cursor):
+  def _update_prices_table_and_commit(self, all_prices):
+    logging.info("updating prices table with {} entries".format(len(all_prices)))
+    query = "REPLACE INTO `runelite`.`prices` (item, price, time, fetched_time) VALUES (%s, %s, %s, %s)"
+    args = []
+
     # BUG: likely an issue with local timezone? Doesn't matter that much tho.
-    time = datetime.datetime.fromtimestamp(price["time"]["epochSecond"])
-    fetched_time = datetime.datetime.now()
-    data = (
-      ("item", "%s", price["id"]),
-      ("price", "%s", price["price"]),
-      ("time", "%s", time.strftime("%Y-%m-%d %H:%M:%S")),
-      ("fetched_time", "%s", fetched_time.strftime("%Y-%m-%d %H:%M:%S"))
-    )
+    fetched_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    columns, subs, args = zip(*data)
-    columns = ", ".join(columns)
-    subs = ", ".join(subs)
+    for item_price in all_prices:
+      time = datetime.datetime.fromtimestamp(item_price["time"]["epochSecond"]).strftime("%Y-%m-%d %H:%M:%S")
+      args.append([
+        item_price["id"],
+        item_price["price"],
+        time,
+        fetched_time,
+      ])
 
-    cursor.execute("REPLACE INTO `runelite`.`prices` ({}) values ({})".format(columns, subs), args)
+    with self.dbconn.cursor() as c:
+      rows_changed = c.executemany(query, args)
+
+    self.dbconn.commit()
+    logging.info("updated {} prices rows".format(rows_changed))
 
 
 def main():
   if RL_VERSION == "none":
     raise RuntimeError("must first install runelite before running this")
 
-  logging.basicConfig(format="[%(asctime)s][%(levelname)s] %(message)s", datefmt="%Y-%m-%d %H:%M:%S", level=logging.DEBUG)
+  logging.basicConfig(format="[%(asctime)s][%(levelname)s] %(message)s", datefmt="%Y-%m-%d %H:%M:%S", level=logging.INFO)
   if len(sys.argv) < 2:
     print("error: must specify action as either seed or fetch", file=sys.stderr)
     sys.exit(1)
